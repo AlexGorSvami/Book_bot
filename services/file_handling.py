eb5223ab3a5599555f92595b482551cae074f4b8
AlexGorSvami <jagger15696@gmail.com>
2023-05-21 21:32:56 +0300

    create function _get_part_text and prepare_book

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,19 +1,32 @@
-import re
-
-BOOK_PATH = 'book/besy.txt'
+BOOK_PATH = '/home/alex/Book_bot/book/besy.txt'
 PAGE_SIZE = 1050
 
 book: dict[int, str] = {}
 
 #Функция, возвращающая строку с текстом страницы и её размер
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-    edit_text = re.sub(r'[.,!?:;]\.+$', '', text[start:start + size])
-    edit_text = re.findall(r'(?s).+[.,!?:;]', edit_text)
-    return *edit_text, len(*edit_text)
+    def _get_part_text(text, start, size):
+        symbols = ',.!:;?'
+        end = start + size
+        if text[start:end][-1] not in symbols:
+            return _get_part_text(text, start, size - 1)
+        if text[start:end + 1][-1] in symbols:
+            if text[start:end + 1] != text[start:end]:
+                return _get_part_text(text, start, size - 1)
+            return text[start:end], len(text[start:end])
+        return text[start:end], size
+
 
 #Функция, форматирующая словарь книги
 def prepare_book(path: str) -> None:
-    pass
+    with open(path, 'r') as file:
+        text = file.read()
+    start, page_number = 0, 1
+    while start < len(text):
+        page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
+        start += page_size
+        book[page_number] = page_text.strip()
+        page_number += 1
 
 #Вызов функции prepare_book для подготовки книги из текстового файла
 prepare_book(BOOK_PATH)
